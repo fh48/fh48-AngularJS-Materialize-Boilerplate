69e0cc14e5f362f776035bd8e8e813c85a3b2ec4
fh48 <fh48@St-andrews.ac.uk>
2017-09-03 15:25:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let anyApp = angular.module('anyApp',['ngResource','ui.materialize']);
+
+ anyApp.controller('homeCtrl', ['$scope', '$resource', function($scope, $resource ){
+
+  $scope.x = "x";
+  $scope.y = "y";
+  $scope.hello = "hello worlds"
+  console.log("lol")
+
+
+
+}]);
