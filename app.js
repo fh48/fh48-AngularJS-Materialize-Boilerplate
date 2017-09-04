99e8716c3b53ab264fcd5dcee660ccbab105e05c
fh48 <fh48@St-andrews.ac.uk>
2017-09-04 10:04:19 +0200

    changed directory name

--- app.js
+++ app.js
@@ -2,11 +2,21 @@
 
 let anyApp = angular.module('anyApp',['ngResource','ui.materialize']);
 
- anyApp.controller('homeCtrl', ['$scope', '$resource', function($scope, $resource ){
+ anyApp.controller('homeCtrl', ['$scope', '$resource','$http', function($scope, $resource, $http ){
 
   $scope.x = "x";
   $scope.y = "y";
-  $scope.hello = "hello worlds"
+  $scope.hello = "hello world"
+
+  $scope.deploy = function(){
+
+    let URL = "http://localhost:8080/engine-rest/deployment/"
+
+    let deployment = $resource(URL).get
+
+  }
+
+
 
   $scope.print =  function(){
     console.log("lol")
